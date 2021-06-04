20b3d7b30476493fb64f12363b7501e8b0f0474b
Yander <ysantana@fidenslat.com>
2021-06-03 19:48:44 -0500

    cambiando base

--- src/app/servicios/pago-proveedor.service.ts
+++ src/app/servicios/pago-proveedor.service.ts
@@ -7,8 +7,8 @@ import { Router } from "@angular/router";
 })
 export class PagoProveedorService {
   //private URL = 'http://localhost:3000/pagoProveedor'; //localhost
-  private URL = "http://104.248.14.190:3000/pagoProveedor";
-  //private URL = 'http://104.131.82.174:3000/pagoProveedor';
+  //private URL = "http://104.248.14.190:3000/pagoProveedor";
+  private URL = 'http://104.131.82.174:3000/pagoProveedor';
   constructor(public http: HttpClient, public router: Router) {}
 
   newPagoProveedor(pagoP) {

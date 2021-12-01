2802ca070d7d8c8c568fdaebac5e687df9303e31
Yander <ysantana@fidenslat.com>
2021-11-30 23:05:24 -0500

    ajuste nueva base

--- src/app/servicios/reciboCaja.service.ts
+++ src/app/servicios/reciboCaja.service.ts
@@ -7,8 +7,8 @@ import { Router } from "@angular/router";
 })
 export class ReciboCajaService {
   //private URL = "http://104.248.14.190:3000/reciboCaja";
-  //private URL = "http://104.131.82.174:3000/reciboCaja";
-  private URL = 'http://localhost:3000/reciboCaja'; //localhost
+  private URL = "http://104.131.82.174:3000/reciboCaja";
+  //private URL = 'http://localhost:3000/reciboCaja'; //localhost
 
   constructor(public http: HttpClient, public router: Router) {}
 

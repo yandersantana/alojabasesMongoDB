dfb244ad93f1e5b67478ad20ad82395d4479b3bc
Yander <ysantana@fidenslat.com>
2022-03-05 18:11:46 -0500

    actualizando pruebas

--- src/app/servicios/parametrizaciones.service.ts
+++ src/app/servicios/parametrizaciones.service.ts
@@ -7,8 +7,8 @@ import { Router } from "@angular/router";
 })
 export class ParametrizacionesService {
   //private URL = 'http://localhost:3000/parametrizaciones'; //localhost
- // private URL = "http://159.223.107.115:3000/parametrizaciones";
-  private URL = 'http://104.131.82.174:3000/parametrizaciones';
+  private URL = "http://159.223.107.115:3000/parametrizaciones";
+  //private URL = 'http://104.131.82.174:3000/parametrizaciones';
   constructor(public http: HttpClient, public router: Router) {}
 
   newParametrizacion(parametrizacion) {

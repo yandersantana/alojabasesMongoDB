f57936402309fb8f4987c9ff16003250b2d00a91
Yander <ysantana@fidenslat.com>
2022-03-05 11:19:45 -0500

    subiendo cambios a produccion

--- src/app/servicios/cuentasPorCobrar.service.ts
+++ src/app/servicios/cuentasPorCobrar.service.ts
@@ -7,8 +7,8 @@ import { Router } from "@angular/router";
 })
 export class CuentasPorCobrarService {
   //private URL = "http://localhost:3000/cuentaPorCobrar"; //localhost
-  //private URL = "http://104.131.82.174:3000/cuentaPorCobrar";
-  private URL = "http://159.223.107.115:3000/cuentaPorCobrar";
+  private URL = "http://104.131.82.174:3000/cuentaPorCobrar";
+  //private URL = "http://159.223.107.115:3000/cuentaPorCobrar";
   constructor(public http: HttpClient, public router: Router) {}
 
   newCuentaPorCobrar(cuenta) {

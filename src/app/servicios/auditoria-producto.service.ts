e95cb3a5d358c0f34e300ccc34edbca582e39988
Yander <ysantana@fidenslat.com>
2022-05-30 19:47:08 -0500

    publicacion entorno produccion

--- src/app/servicios/auditoria-producto.service.ts
+++ src/app/servicios/auditoria-producto.service.ts
@@ -7,8 +7,8 @@ import { Router } from "@angular/router";
 })
 export class AuditoriaProductoService {
   //private URL = 'http://localhost:3000/auditoriasProductos'; //localhost
-  //private URL = "http://104.131.82.174:3000/auditoriasProductos";
-  private URL = "http://159.223.107.115:3000/auditoriasProductos"; //localhost
+  private URL = "http://104.131.82.174:3000/auditoriasProductos";
+  //private URL = "http://159.223.107.115:3000/auditoriasProductos"; //localhost
 
   constructor(public http: HttpClient, public router: Router) {}
 

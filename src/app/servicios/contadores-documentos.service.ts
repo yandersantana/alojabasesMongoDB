59272af86f95fe5ad9f9d8cedc80456bd5dce2b4
Yander <ysantana@fidenslat.com>
2022-06-04 13:46:58 -0500

    ajuste cambios a produccion

--- src/app/servicios/contadores-documentos.service.ts
+++ src/app/servicios/contadores-documentos.service.ts
@@ -7,8 +7,8 @@ import { Router } from "@angular/router";
 })
 export class ContadoresDocumentosService {
   //private URL = 'http://localhost:3000/contadores'; //localhost
-  private URL = "http://159.223.107.115:3000/contadores";
-  //private URL = "http://104.131.82.174:3000/contadores";
+  //private URL = "http://159.223.107.115:3000/contadores";
+  private URL = "http://104.131.82.174:3000/contadores";
 
   constructor(public http: HttpClient, public router: Router) {}
 

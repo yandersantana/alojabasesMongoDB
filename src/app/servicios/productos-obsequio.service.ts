b31377d23c70ddcd341f1ed3bfab63199da85c9c
Yander <ysantana@fidenslat.com>
2022-08-16 15:21:50 -0500

    ajustes de settings para produccion

--- src/app/servicios/productos-obsequio.service.ts
+++ src/app/servicios/productos-obsequio.service.ts
@@ -7,8 +7,8 @@ import { Router } from "@angular/router";
 })
 export class ProductosObsequioService {
   //private URL = 'http://localhost:3000/productosObsequio'; //localhost
-  private URL = "http://159.223.107.115:3000/productosObsequio";
-  //private URL = 'http://104.131.82.174:3000/productosObsequio';
+  //private URL = "http://159.223.107.115:3000/productosObsequio";
+  private URL = 'http://104.131.82.174:3000/productosObsequio';
   constructor(public http: HttpClient, public router: Router) {}
 
   newProductoObsequio(productoObs) {

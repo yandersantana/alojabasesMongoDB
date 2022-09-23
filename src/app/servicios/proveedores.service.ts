ab4f236b5f1a737c6d667bcb641a443b7f1b6506
Yander <ysantana@fidenslat.com>
2022-09-23 09:16:19 -0500

    actualizacion produccion correccion link

--- src/app/servicios/proveedores.service.ts
+++ src/app/servicios/proveedores.service.ts
@@ -7,8 +7,8 @@ import { Router } from "@angular/router";
 })
 export class ProveedoresService {
   //private URL = 'http://localhost:3000/proveedores'; //localhost
-  private URL = "http://159.223.107.115:3000/proveedores";
-  //private URL = 'http://104.131.82.174:3000/proveedores';
+  //private URL = "http://159.223.107.115:3000/proveedores";
+  private URL = 'http://104.131.82.174:3000/proveedores';
   constructor(public http: HttpClient, public router: Router) {}
 
   newProveedor(proveedor) {

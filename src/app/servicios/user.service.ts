680dbfa6682c2efe1c1afd1ad6ba6f16ac3cab75
Yander <ysantana@fidenslat.com>
2022-12-05 22:47:53 -0500

    ajuste PDF

--- src/app/servicios/user.service.ts
+++ src/app/servicios/user.service.ts
@@ -10,8 +10,8 @@ export class UserService {
   role = "";
   userEmail = "";
   //private URL = 'http://localhost:3000/usuario';
-  private URL = "http://159.223.107.115:3000/usuario";
-  //private URL = 'http://104.131.82.174:3000/usuario';
+  //private URL = "http://159.223.107.115:3000/usuario";
+  private URL = 'http://104.131.82.174:3000/usuario';
   constructor(private http: HttpClient,) {}
 
   newUser(user) {

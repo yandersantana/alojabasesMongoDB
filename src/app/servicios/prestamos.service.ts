0edbfa3b1931906d8bb72fed7f3801c34fdb0e38
Yander <ysantana@fidenslat.com>
2023-04-10 22:33:54 -0500

    subida de correcciones a produccion

--- src/app/servicios/prestamos.service.ts
+++ src/app/servicios/prestamos.service.ts
@@ -7,8 +7,8 @@ import { Router } from "@angular/router";
 })
 export class PrestamosService {
   //private URL = "http://localhost:3000/prestamos"; //localhost
-  //private URL = "http://104.131.82.174:3000/prestamos";
-  private URL = "http://159.223.107.115:3000/prestamos";
+  private URL = "http://104.131.82.174:3000/prestamos";
+  //private URL = "http://159.223.107.115:3000/prestamos";
   constructor(public http: HttpClient, public router: Router) {}
 
   newPrestamo(prestamo) {
